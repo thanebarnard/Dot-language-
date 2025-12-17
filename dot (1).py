28fc9906ee5b6e0c121f49f70cb96b86b2bf1616
thanebarnard <thanebarnard@gmail.com>
2025-12-17 11:25:07 -0500

    Add files via upload
    This is the programing language I made

---
+++ dot (1).py
@@ -0,0 +1,565 @@
+def a():
+  return "a"
+def b():
+  return "b"
+def c():
+  return "c"
+def d():
+  return "d"
+def e():
+  return "e"
+def f():
+  return "f"
+def g():
+  return "g"
+def h():
+  return "h"
+def i():
+  return "i"
+def j():
+  return "j"
+def k():
+  return "k"
+def l():
+  return "l"
+def m():
+  return "m"
+def n():
+  return "n"
+def o():
+  return "o"
+def p():
+  return "p"
+def q():
+  return "q"
+def r():
+  return "r"
+def s():
+  return "s"
+def t():
+  return "t"
+def u():
+  return "u"
+def v():
+  return "v"
+def w():
+  return "w"
+def x():
+  return "x"
+def y():
+  return "y"
+def z():
+  return "z"
+def upa():
+  input(" ")
+def upb():
+  input(" ")
+  input(" ")
+def upc():
+  input(" ")
+  input(" ")
+  input(" ")
+def upd():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upe():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upf():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upg():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def uph():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upi():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upj():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upk():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upl():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upm():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upn():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upo():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upp():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upq():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upr():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def ups():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upt():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upu():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upv():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upw():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upx():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upy():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def upz():
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+  input(" ")
+def end():
+  input("    ")
+def clear():
+  input("<<<MATHLIB///LASTCHECK*$@tb")
+def graph():
+  input("""
+  00000
+  00000
+  00000
+  00000
+  00000
+  """)
+def googolplexplusone():
+  input("10**(10**100) + 1")
+def googolplexminusone():
+  input("10**(10**100) - 1")
+def trtenplusone():
+  input("11")
+def trtenplustwo():
+  input("12")
+def trtenplusthree():
+  input("13")
+def compare():
+  input("ROUND")
+  input("1 => 0")
+  input("by 10")
+  input("5 or more is 10")
+def equalone():
+  input("1")
+def equaltwo():
+  input("2")
+def equalhighnumberbiggerthanten():
+  input("<<<LARGER^TEN***")
+def oneplusone():
+  input("2")
+def twoplusone():
+  input("3")
+def threeplusone():
+  input("4")
+def fourplusone():
+  input("5")
+def onetimesone():
+  input("1")
+def onetimestwo():
+  input("2")
+def onetimesthree():
+  input("3")
+def onetimesfour():
+  input("4")
+def onetimesfive():
+  input("5")
+def onetimessix():
+  input("ERROR>LIM>5")
+def me():
+  input("42")
+commands = {
+  ".": a,
+  "..": b,
+  "...": c,
+  "....": d,
+  ".....": e,
+  "......": f,
+  ".......": g,
+  "........": h,
+  ".........": i,
+  "..........": j,
+  "...........": k,
+  "............": l,
+  ".............": m,
+  "..............": n,
+  "...............": o,
+  "................": p,
+  ".................": q,
+  "..................": r,
+  "...................": s,
+  "....................": t,
+  ".....................": u,
+  "......................": v,
+  ".......................": w,
+  "........................": x,
+  ".........................": y,
+  "..........................": z,
+  "..................... ................ a": upa,
+  "..................... ................ b": upb,
+  "..................... ................ c": upc,
+  "..................... ................ d": upd,
+  "..................... ................ e": upe,
+  "..................... ................ f": upf,
+  "..................... ................ g": upg,
+  "..................... ................ h": uph,
+  "..................... ................ i": upi,
+  "..................... ................ j": upj,
+  "..................... ................ k": upk,
+  "..................... ................ l": upl,
+  "..................... ................ m": upm,
+  "..................... ................ n": upn,
+  "..................... ................ o": upo,
+  "..................... ................ p": upp,
+  "..................... ................ q": upq,
+  "..................... ................ r": upr,
+  "..................... ................ s": ups,
+  "..................... ................ t": upt,
+  "..................... ................ u": upu,
+  "..................... ................ v": upv,
+  "..................... ................ w": upw,
+  "..................... ................ x": upx,
+  "..................... ................ y": upy,
+  "..................... ................ z": upz,
+  "a + a": oneplusone,
+  "b + a": twoplusone,
+  "c + a": threeplusone,
+  "a|a": onetimesone,
+  "a|b": onetimestwo,
+  "a|c": onetimesthree,
+  "a|d": onetimesfour,
+  "a|e": onetimesfive,
+  "a|f": onetimessix,
+  "bten": equalhighnumberbiggerthanten,
+  "a": equalone,
+  "^": clear,
+  "*": end,
+  "?": graph,
+  "(": googolplexplusone,
+  ")": googolplexminusone,
+}
+while True:
+    user_input = input("<<<")
+    for cmd in user_input.replace("\n", ";").split(";"):
+        cmd = " ".join(cmd.strip().split())
+        if not cmd:
+            continue
+        if cmd not in commands:
+            continue
+        respond = commands[cmd]
+        respond()
